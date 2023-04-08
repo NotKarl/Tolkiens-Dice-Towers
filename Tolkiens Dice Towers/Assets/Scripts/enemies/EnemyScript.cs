0c1f55411ed7390b8236bf83c20b28826372455a
NyraSaphira <pattybella@gmx.de>
2023-04-08 19:48:43 +0200

    enemy script changes

--- Tolkiens Dice Towers/Assets/Scripts/enemies/EnemyScript.cs
+++ Tolkiens Dice Towers/Assets/Scripts/enemies/EnemyScript.cs
@@ -1,21 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Unity.Engine.AI; //for moving (?)
 
 public class EnemyScript : MonoBehaviour
 {
 
     abstract class Enemy
     {
+
         internal static int count = 0;
         internal int hp = 1;
-        internal int speed = 1;
         internal int ap = 1;
+        internal int speed = 1;
+        internal bool alive;
 
         public Enemy()
         {                                                     //constructor
             count++;
-            Debug.Log($"Enemy created! There are {count} enemie(s)!");
+            Debug.Log($"Enemy created! There are/is {count} enemie(s)!");
+            alive = true;
+
         }
 
         public virtual void TakeDamage(int damage)
@@ -24,14 +29,35 @@ public class EnemyScript : MonoBehaviour
             Debug.Log($"I got {damage} damage!");
             Debug.Log($"I have {hp} hp left!");
 
-            if (hp == 0)
-            {                                                    //death
-                count--;
-                Debug.Log($"Enemy dies! There are {count} enemie(s) left!");
-                //death animation start, few seconds later enemy object destroyed
-            }
+            if (hp == 0) Death();
+        }
+
+        public virtual void Death()
+        {
+            count--;
+            Debug.Log($"Enemy dies! There are/is {count} enemie(s) left!");
+            alive = false;
+
+            //death animation, few seconds later enemy object destroyed //different death animation (explosion?) if (lastWaypointIsReached) ?
+            //Destroy(gameObject);
+        }
+
+        public virtual void Attack()
+        {
+            //play attack animation (explosion?);
+            //hp of tower - the ap of enemy
+            Death();
+        }
+
+        public virtual void Walk()
+        {
+            //play walk animation
+            //move along path with speed
+
         }
     }
+
+
 
     class Troll : Enemy
     {
@@ -57,15 +83,21 @@ public class EnemyScript : MonoBehaviour
             enemy.TakeDamage(5);
         }
 
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        // if in range of wall -> start attack animation
-        // if hp == 0 -> death animation    
-        // else -> walk animation, following path
+        /* while (alive)                   
+           {
+               if (lastWaypointIsReached)
+                  Enemy.Attack();
+               else if (hitByTowerShot)
+                  Enemy.TakeDamage();
+               else 
+                  Enemy.Walk(); 
+                                           //https://www.youtube.com/watch?v=I3sY3I_f5_o  tut for animation implementation
+           }
+         */
     }
 }
-
