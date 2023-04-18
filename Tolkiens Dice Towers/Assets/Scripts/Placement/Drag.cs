5e0bb72110d8b07f3866a2f42ba4da4e4e145903
DenisFeler <106584082+DenisFeler@users.noreply.github.com>
2023-04-18 16:48:06 +0200

    Hit Range and Color Coding
    Color the tower depending on the possibility to place it.
    
    Added a hit range on dragging the tower to indicate the reach of it's attacking

--- Tolkiens Dice Towers/Assets/Scripts/Placement/Drag.cs
+++ Tolkiens Dice Towers/Assets/Scripts/Placement/Drag.cs
@@ -4,53 +4,116 @@ using UnityEngine;
 
 public class Drag : MonoBehaviour
 {
+    //Positioning Variables
     [SerializeField] public Camera _camera;
-    
+    private Vector3 offset;
+
+    //Placement Checks
     public bool dragging = false;
     public bool _placed = false;
-    private Vector3 offset;
+
+    //Collision Checks
     private bool _collPath = false;
-    private bool _collTower = false;
 
-    private SpriteRenderer _collColor;
+    //Color + Sprite to Color
+    public Color defCol;
+    public Color placeCol;
+    public Color nonPlaceCol;
+    private SpriteRenderer towSprite;
 
+    //Tower Range indicator
+    private bool createdRange = false;
+    public GameObject hitRange;
+
+    private void Start()
+    {
+        towSprite = GetComponent<SpriteRenderer>();
+
+        defCol = towSprite.color;
+    }
+    
     private void Update()
     {
+        //Object position to Mouse position
         if (dragging)
         {
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + offset;
+            hitRange.transform.position = transform.position;
+        }
+
+        //Color on start
+        if (!_placed && !dragging)
+        {
+            towSprite.color = defCol;
+        }
+        
+        //Color on collision
+        if (_collPath && !_placed && dragging)
+        {
+            towSprite.color = nonPlaceCol;
+        }
+        
+        //Color on non collision
+        if (!_collPath && !_placed && dragging)
+        {
+            towSprite.color = placeCol;
+        }
+        
+        //Color on placing
+        if (_placed && !dragging)
+        {
+            towSprite.color = defCol;
         }
     }
     
     private void OnMouseDown()
     {
-        if(!_placed)
+        if (!_placed)
         {
             offset = transform.position - Camera.main.ScreenToWorldPoint(Input.mousePosition);
             dragging = true;
+
+            //Create a range indicator once
+            if (!createdRange)
+            {
+                hitRange = Instantiate(hitRange, transform.position, transform.rotation);
+
+                createdRange = true;
+            }
         }   
     }
 
     private void OnMouseUp()
     {
-        if(!_placed && !_collPath && !_collTower)
+        if (!_placed && !_collPath)
         {
             dragging = false;
             _placed = true;
+
+            towSprite.color = defCol;
         }
         
+        if (_placed && createdRange)
+        {
+            Destroy(hitRange);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
-        Debug.Log("Trigger Entered!");
+        _collPath = true;
+
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
         _collPath = true;
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
-        Debug.Log("Trigger Exited!");
         _collPath = false;
+
     }
 
     public Vector2 GetMousePosition()
